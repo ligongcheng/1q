013749b87861ce3043c19a3e4ba71d82d1eedbc8
ligongcheng <askchengtao@gmail.com>
2018-05-12 00:18:09 +0800

    1

--- proxyTest/src/main/java/cn/it/study/deadlock/Mylock.java
+++
@@ -1,7 +0,0 @@
-package cn.it.study.deadlock;
-
-public class Mylock {
-
-	public static Object objA = new Object();
-	public static Object objB = new Object();
-}
--- proxyTest/src/main/java/cn/it/study/proxy/StudentDao.java
+++
@@ -1,7 +0,0 @@
-package cn.it.study.proxy;
-
-public interface StudentDao {
-	public abstract void login();
-
-	public abstract void regist();
-}
